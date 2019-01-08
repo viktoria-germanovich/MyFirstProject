5d611b419b418d41b77dc0ae40887d91b59a3961
Viktoryia Hermanovich <gerviku@gmail.com>
2019-01-09 01:03:30 +0300

    Initial commit.

--- ForthHomework/task2/task2.js
+++ ForthHomework/task2/task2.js
@@ -1,12 +1,11 @@
-homePage.onclick = function() {
+initialPage.onclick = function () {
     document.location.href = "home.html"
 }
-flexBox.onclick = function() {
-    document.body.innerHTML = '';
-    load_layout();
-    }
-
-    function load_layout()
-    {
-
-    }
+flexBox.onclick = function () {
+    var elem = document.getElementById('content');
+    elem.parentNode.removeChild(elem);
+    var div = document.createElement('div');
+    div.className = "flex-container";
+    div.innerHTML = "<div>1</div> <div>2</div> <div>3</div> <div>4</div> <div>5</div> <div>6</div>";
+    document.body.appendChild(div);
+}
