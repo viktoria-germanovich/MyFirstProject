a24896d357e232ea8d93876f5d79b814d7964cbd
Viktoryia Hermanovich <gerviku@gmail.com>
2019-01-17 02:17:59 +0300

    commit

--- Homework-6/script.js
+++ Homework-6/script.js
@@ -1,25 +1,49 @@
-function loadXMLRequest() {
+function getRequest() {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://github.com/viktoria-germanovich/MyFirstProject/blob/master/Homework-6/json.json', false);
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', false);
     xhr.send();
     if (xhr.status != 200) {
         alert(xhr.status + ': ' + xhr.statusText);
     } else {
-        createTable(xhr);
+        var array = JSON.parse(xhr.responseText);
+        var table = '<table>';
+        table += '<th>User ID</th><th>ID</th><th>Title</th><th>Body</th><th>Delete</th>';
+        for (var i in array) {
+            table += '<tr><td>' +
+                array[i]['userId'] + '</td><td>' +
+                array[i]['id'] + '</td><td>' +
+                array[i]['title'] + '</td><td>' +
+                array[i]['body'] + '</td><td><button id=delete>Delete</button></td></tr>';
+                
+        }
+        table += '</table>';
+        document.getElementById('main').innerHTML = table;
     }
 }
 
-function createTable(xhr) {
-    var array = JSON.parse(xhr.responseText);
-    var table = '<table>';
-    table += '<th>User ID</th><th>ID</th><th>Title</th><th>Body</th>';
-    for (var i in array) {
-        table += '<tr><td>' +
-            array[i]['userId'] + '</td><td>' +
-            array[i]['id'] + '</td><td>' +
-            array[i]['title'] + '</td><td>' +
-            array[i]['body'] + '</td></tr>';
+
+function postRequest() {
+
+    var xhr = new XMLHttpRequest();
+
+    var obj = {};
+    obj.userID =  document.getElementById('userID').value;
+    obj.title =  document.getElementById('title').value;
+    obj.body =  document.getElementById('body').value;
+    var json = JSON.stringify(obj);
+  
+    xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(json);
+      
+    xhr.onload = function () {
+      if (xhr.readyState == 4 && xhr.status == "201") {
+      alert(xhr.responseText);
+      }
     }
-    table += '</table>';
-    document.getElementById('main').innerHTML = table;
-}
+  
+    xhr.onerror = function () {
+      alert('Ошибка:' + xhr.status);
+    }  
+  }
+
