3e70f058ecaeb1bf5d0875ed3deefcab665a842b
Viktoryia Hermanovich <v.hermanovich@LeverX.local>
2019-01-17 15:28:58 +0300

    json is added

--- Homework-6/script.js
+++ Homework-6/script.js
@@ -1,25 +1,43 @@
 function getRequest() {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts', false);
+    xhr.open('GET', 'http://localhost:3000/posts', false);
     xhr.send();
     if (xhr.status != 200) {
         alert(xhr.status + ': ' + xhr.statusText);
     } else {
         var array = JSON.parse(xhr.responseText);
         var table = '<table>';
-        table += '<th>User ID</th><th>ID</th><th>Title</th><th>Body</th><th>Delete</th>';
+        table += '<th>User ID</th><th>ID</th><th>Title</th><th>Body</th><th>Delete</th><th>Edit</th>';
         for (var i in array) {
             table += '<tr><td>' +
                 array[i]['userId'] + '</td><td>' +
                 array[i]['id'] + '</td><td>' +
                 array[i]['title'] + '</td><td>' +
-                array[i]['body'] + '</td><td><button id=delete>Delete</button></td></tr>';
-                
+                array[i]['body'] +
+                '</td><td><button id="' + array[i]['id'] + '" onclick="delRequest(this)">Delete</button></td>' +
+                '</td><td><button id="' + array[i]['id'] + '" onclick="putRequest(this)">Edit</button></td></tr>';
+
         }
         table += '</table>';
         document.getElementById('main').innerHTML = table;
     }
 }
+
+function delRequest(obj) {
+var url = "http://localhost:3000/posts";
+var xhr = new XMLHttpRequest();
+xhr.open("DELETE", url, true);
+xhr.onload = function () {
+	var users = JSON.parse(xhr.responseText);
+	if (xhr.readyState == 4 && xhr.status == "200") {
+		console.table(users);
+	} else {
+		console.error(users);
+	}
+}
+xhr.send(null);
+}
+
 
 
 function postRequest() {
@@ -27,23 +45,47 @@ function postRequest() {
     var xhr = new XMLHttpRequest();
 
     var obj = {};
-    obj.userID =  document.getElementById('userID').value;
-    obj.title =  document.getElementById('title').value;
-    obj.body =  document.getElementById('body').value;
+    obj.userId = document.getElementById('userId').value;
+    obj.title = document.getElementById('title').value;
+    obj.body = document.getElementById('body').value;
     var json = JSON.stringify(obj);
-  
-    xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts');
+
+    xhr.open('POST', 'http://localhost:3000/posts');
     xhr.setRequestHeader('Content-type', 'application/json');
     xhr.send(json);
-      
-    xhr.onload = function () {
-      if (xhr.readyState == 4 && xhr.status == "201") {
-      alert(xhr.responseText);
-      }
-    }
-  
-    xhr.onerror = function () {
-      alert('Ошибка:' + xhr.status);
-    }  
-  }
 
+    xhr.onload = function () {
+        if (xhr.readyState == 4 && xhr.status == "201") {
+            alert(xhr.responseText);
+        }
+    }
+
+    xhr.onerror = function () {
+        alert('Ошибка:' + xhr.status);
+    }
+}
+
+
+function putRequest() {
+    // var url = "http://localhost:3000/posts";
+
+    // var obj = {};
+    // obj.userId = document.getElementById('userId').value;
+    // obj.title = document.getElementById('title').value;
+    // obj.body = document.getElementById('body').value;
+    // var json = JSON.stringify(obj);
+
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("PUT", url + '/12', true);
+    // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    // xhr.send(json);
+    // xhr.onload = function () {
+    //     if (xhr.readyState == 4 && xhr.status == "201") {
+    //         alert(xhr.responseText);
+    //     }
+    // }
+    // xhr.onerror = function () {
+    //     alert('Ошибка:' + xhr.status);
+    // }
+    
+}
